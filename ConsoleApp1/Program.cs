c848378a1a87da7817b3fbc0a419a332ba058bde
mohamadalitellawi <mohamadalitellawi@gmail.com>
2019-04-18 15:06:30 +0300

    add WR

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,6 +19,8 @@ namespace ConsoleApp1
             Console.WriteLine("Another line.");
             Console.ResetColor();
 
+            Console.WriteLine("Hello");
+
             Console.Read();
         }
     }
