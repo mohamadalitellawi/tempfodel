c52802b2326d3fc95f63b1292fa4dc0ab97db820
mohamadalitellawi <mohamadalitellawi@gmail.com>
2019-04-18 15:05:07 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("White on blue.        " + typeof(ConsoleColor));
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Another line.");
+            Console.ResetColor();
+
+            Console.Read();
+        }
+    }
+}
